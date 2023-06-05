f35b70c90730e5febf43edfe32f2c1a76e3c4718
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2023-06-05 15:55:56 +0200

    build: add tmpfiles configuration for lightdm

--- scripts/post/graphical-target.sh
+++ scripts/post/graphical-target.sh
@@ -6,4 +6,6 @@ set -oue pipefail
 systemctl set-default graphical.target
 systemctl enable lightdm
 
+echo "d /var/lib/lightdm 750 lightdm lightdm -" > /usr/lib/tmpfiles.d/lightdm.conf
+
 # plymouth-set-default-theme elementary -R
